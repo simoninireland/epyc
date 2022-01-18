da2d03b59f811152cb54024edec7222f73cd93f8
Simon Dobson <simon.dobson@computer.org>
2022-01-18 15:00:54 +0000

    Removed explicit logging setup

--- test/test_labs.py
+++ test/test_labs.py
@@ -23,12 +23,6 @@ import numpy
 from epyc import *
 
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(Logger)
-logger.setLevel(logging.DEBUG)
-
-
 class SampleExperiment(Experiment):
     '''A very simple experiment that adds up its parameters.'''
 
